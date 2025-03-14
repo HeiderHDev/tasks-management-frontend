7419dc53682b3aa7b60a9924659377ae4bdada6f
HeiderHDev <heiderhrnndz@gmail.com>
2025-03-14 10:57:00 -0500

    feat: 🎸 Se realiza el diseño del Layout del poryecto

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { MediaMatcher } from '@angular/cdk/layout';
+
+const materialModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatSidenavModule,
+  MatListModule,
+];
+
+interface MenuItem {
+  link: string;
+  label: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, ...materialModules],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  public mobileQuery!: MediaQueryList;
+  public menuItems: MenuItem[] = [];
+  private _mobileQueryListener: () => void;
+
+  constructor() {
+    const changeDetectorRef = inject(ChangeDetectorRef);
+    const media = inject(MediaMatcher);
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  public ngOnInit(): void {
+    this.menuItems = [
+      { link: 'tareas', label: 'Tareas', icon: 'task' },
+      { link: 'prueba', label: 'prueba', icon: 'sync' },
+    ];
+  }
+
+  public ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
+}
