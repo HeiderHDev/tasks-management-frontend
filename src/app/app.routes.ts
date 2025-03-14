561d827c90ee2bff007e97e06f3322d628b6e9ac
HeiderHDev <heiderhrnndz@gmail.com>
2025-03-14 10:15:12 -0500

    feat: 🎸 Se define las rutas del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TaskComponent } from '@task/task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/layout.component').then((m) => m.LayoutComponent),
+    children: [
+      { path: 'tareas', component: TaskComponent },
+      { path: '**', redirectTo: 'tareas' },
+    ],
+  },
+];
