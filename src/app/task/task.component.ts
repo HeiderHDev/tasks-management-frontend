a8c21503403a03d5415ccf9c19d24a2385432816
HeiderHDev <heiderhrnndz@gmail.com>
2025-03-14 10:57:45 -0500

    feat: 🎸 Se crea boton de crear tarea

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
+
+const materialModules = [MatButtonModule];
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ...materialModules],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  styleUrl: './task.component.scss',
+  providers: [provideNativeDateAdapter()],
 })
 export class TaskComponent {
+  public date = new Date();
+  private matDialog = inject(MatDialog);
 
+  public openTaskFormModal() {}
 }
