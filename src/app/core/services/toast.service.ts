93f798798cebd0973ffe3fce5724ce63b5714568
HeiderHDev <heiderhrnndz@gmail.com>
2025-03-15 10:39:20 -0500

    feat: 🎸 Se crea toastService

---
+++ src/app/core/services/toast.service.ts
@@ -0,0 +1,41 @@
+import { NgxToastService } from '@angular-magic/ngx-toast';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  private _ngxToastService = inject(NgxToastService);
+
+  public success(title: string, message: string): void {
+    this._ngxToastService.success({
+      title,
+      messages: [message],
+      delay: 10000,
+    });
+  }
+
+  public error(title: string, message: string): void {
+    this._ngxToastService.error({
+      title,
+      messages: [message],
+      delay: 10000,
+    });
+  }
+
+  public warning(title: string, message: string): void {
+    this._ngxToastService.warning({
+      title,
+      messages: [message],
+      delay: 10000,
+    });
+  }
+
+  public info(title: string, message: string): void {
+    this._ngxToastService.info({
+      title,
+      messages: [message],
+      delay: 10000,
+    });
+  }
+}
