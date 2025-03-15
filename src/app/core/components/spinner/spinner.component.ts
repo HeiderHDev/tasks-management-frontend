828534ea9559e80ffa124c639b9d107aaf5daa62
HeiderHDev <heiderhrnndz@gmail.com>
2025-03-15 10:38:10 -0500

    feat: 🎸 Se crea componente spinner

---
+++ src/app/core/components/spinner/spinner.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  imports: [MatProgressSpinnerModule],
+  templateUrl: './spinner.component.html',
+  styleUrl: './spinner.component.scss',
+})
+export class SpinnerComponent {
+  public loading = input<boolean>(false);
+}
