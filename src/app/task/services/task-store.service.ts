cf2dc1800e10a24e14c927a2b4e100112beadb8b
HeiderHDev <heiderhrnndz@gmail.com>
2025-03-16 16:16:29 -0500

    feat: 🎸 Metodos para cargar tareas y crear tareas centralizada

--- src/app/task/services/task-store.service.ts
+++ src/app/task/services/task-store.service.ts
@@ -2,6 +2,8 @@ import { inject, Injectable, signal } from '@angular/core';
 import { TaskService } from './task.service';
 import { Task } from '@task/interfaces/task.interface';
 import { PaginationDto } from '@core/models/pagination-dto.interface';
+import { finalize } from 'rxjs';
+import { CreateTask } from '@task/interfaces/create-task.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,7 @@ import { PaginationDto } from '@core/models/pagination-dto.interface';
 export class TaskStoreService {
   private readonly _taskService = inject(TaskService);
 
-  public task = signal<Task[]>([]);
+  public tasks = signal<Task[]>([]);
   public totalRecords = signal<number>(0);
   public currentPagination = signal<PaginationDto>({
     Page: 1,
@@ -17,4 +19,24 @@ export class TaskStoreService {
   });
 
   constructor() {}
+
+  public loadTasks(pagination?: PaginationDto): void {
+    if (pagination) {
+      this.currentPagination.set(pagination);
+    }
+    this._taskService.getListTask(this.currentPagination()).subscribe({
+      next: (response) => {
+        const taskData = response.body as Task[];
+        const cabecera = response.headers.get(
+          'cantidad-total-registros'
+        ) as string;
+        this.tasks.set(taskData);
+        this.totalRecords.set(parseInt(cabecera, 10));
+      },
+    });
+  }
+
+  public createTask(taskData: CreateTask) {
+    return this._taskService.createTask(taskData);
+  }
 }
