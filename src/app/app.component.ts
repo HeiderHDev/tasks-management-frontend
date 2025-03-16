25c2b48ffd8306976976cde370fe78d09048daf6
HeiderHDev <heiderhrnndz@gmail.com>
2025-03-16 08:29:48 -0500

    feat: 🎸 Se implementa spinner y toast en el app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { NgxToastModule } from '@angular-magic/ngx-toast';
+import {
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpinnerComponent } from '@core/components/spinner/spinner.component';
+import { LoadingService } from '@core/services/loading.service';
+
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SpinnerComponent, NgxToastModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'task-management-frontend';
+export class AppComponent implements OnInit {
+  public title = 'task-management-frontend';
+  public loading = false;
+  private readonly _loadingService = inject(LoadingService);
+  private readonly cdr = inject(ChangeDetectorRef);
+  private readonly destroyRef = inject(DestroyRef);
+
+  public ngOnInit(): void {
+    this.listenToLoading();
+  }
+
+  private listenToLoading(): void {
+    this._loadingService
+      .isLoading()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((loading) => {
+        this.loading = loading;
+        this.cdr.detectChanges();
+      });
+  }
 }
