da9291e6b9b5df0cfbddd658a0deaded0408ddfe
HeiderHDev <heiderhrnndz@gmail.com>
2025-03-16 08:19:40 -0500

    feat: 🎸 Se crea metodos para obtener las tareas y crear tareas

--- src/app/task/task.service.ts
+++ src/app/task/task.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, Signal } from '@angular/core';
+import { environment } from '@env/environment';
+import { Observable } from 'rxjs';
+import { Task } from './interfaces/task.interface';
+import { CreateTask } from './interfaces/create-task.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private apiUrl = environment.apiUrl;
+  private http = inject(HttpClient);
 
-  constructor() { }
+  getListTask(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/api/TaskItems`);
+  }
+
+  createTask(body: CreateTask): Observable<Task> {
+    return this.http.post<Task>(`${this.apiUrl}/api/TaskItems`, body);
+  }
 }
